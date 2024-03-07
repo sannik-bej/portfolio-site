b2ae48149453aef16981c4e09860d85d748df0b5
Sannik Bej <sannikbej35@gmail.com>
2024-03-08 02:42:32 +0530

    New background added, CSS and JS modified.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const swapSpace = document.querySelector("#swap-space");
 const aboutBtn = document.querySelector("#about-btn");
 const secTitle = document.querySelector("#sec-title");
 const desc = document.querySelector("#desc");
+const interests = document.querySelector("#interests");
+const works = document.querySelector("#works");
 const aboutSec = {
   secTitle: "About Me",
   desc: "tech / video game enthusiast. i learn quick, stay organized.",
@@ -15,10 +17,14 @@ function SwapSpace(index) {
     case 1:
       secTitle.innerHTML = aboutSec.secTitle;
       desc.innerText = aboutSec.desc;
+      works.style.display = "none";
+      interests.style.display = "block";
       break;
     case 2:
       secTitle.innerHTML = workSec.secTitle;
       desc.innerText = workSec.desc;
+      interests.style.display = "none";
+      works.style.display = "block";
       break;
     default:
       break;
